67e82ea052399d7cbbde901f88458238f924ce5d
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-23 15:26:41 -0300

    Created a enw game

---
+++ Curso Engenharia de Dados/Engenharia de Dados/Python 1/Games/forca.py
@@ -0,0 +1,9 @@
+def jogar_forca():
+    print("*******************************")
+    print('   Bem vindo no jogo de Forca')
+    print("*******************************")
+
+    print('Fim do jogo')
+
+    if __name__ == '__main__':
+        jogar_forca()
