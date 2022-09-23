555087c882dbdec3bf047e2027d437f21ad07ea6
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-23 15:26:30 -0300

    Added a menu for choosing a game

---
+++ Curso Engenharia de Dados/Engenharia de Dados/Python 1/Games/jogos.py
@@ -0,0 +1,16 @@
+from forca import jogar_forca
+from guess_numbers import jogar_adivinacao
+
+print("*******************************")
+print('     Escolha o seu jogo')
+print("*******************************")
+
+print('1- Forca\n2- Adivinacao')
+jogo = int(input('Qual jogo voce deseja jogar? '))
+
+if jogo == 1:
+    print('Jogando Forca!')
+    jogar_forca()
+elif jogo == 2:
+    print('Jogando Adivinacao')
+    jogar_adivinacao()
