e26ad13df36dc3c0a13b43a8b9da9c52f1ac2d89
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-27 09:23:06 -0300

    New folder for Python 2 course

---
+++ Curso Engenharia de Dados/Engenharia de Dados/Python 2/forca.py
@@ -0,0 +1,10 @@
+# Started developing in Python 1, but will be finished in Python 2
+def jogar_forca():
+    print("*******************************")
+    print('   Bem vindo no jogo de Forca')
+    print("*******************************")
+
+    print('Fim do jogo')
+
+    if __name__ == '__main__':
+        jogar_forca()
