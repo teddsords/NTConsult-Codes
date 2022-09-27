de4082d0ef45b1e1b3dcb025c473cf2ed8da678a
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-27 09:25:49 -0300

    Added guessing game to folder

---
+++ Curso Engenharia de Dados/Engenharia de Dados/Python 2/guess_numbers.py
@@ -0,0 +1,53 @@
+# Jogo de Adivinação criado para o curso de Python: começando com a linguagem da Alura
+
+# Developed in Python 1
+from random import randrange
+
+def jogar_adivinacao():
+    print("*******************************")
+    print('Bem vindo no jogo de Adininação')
+    print("*******************************")
+
+    numero_secreto = randrange(1, 101)
+    pontos = 1000
+    print(numero_secreto)
+
+    print("Qual o nivel de dificuldade?")
+    print("1- Facil\n2- Medio\n3- Dificil")
+    nivel = int(input("Escolha o nivel:"))
+
+    if nivel == 1:
+        tentativas = 20
+    elif nivel == 2:
+        tentativas = 10
+    else:
+        tentativas = 5
+
+    for rodada in range(1,tentativas + 1):
+        print(f"Tentativa: {rodada} de {tentativas}")
+        chute = int(input("Digite um numero entre 1 e 100: "))
+        print("Voce digitou:", chute)
+
+        if chute < 1 or chute > 100:
+            print("Voce deve digitar um numero entre 1 e 100!")
+            continue
+
+        if numero_secreto == chute:
+            print(f"Voce acertou e fez {pontos}!")
+            break
+        else:
+            if chute > numero_secreto:
+                print("Errou!! O seu chute foi maior que o numero secreto")
+            elif chute < numero_secreto:
+                print("Errou!! O seu chute foi menor que o numero secreto")
+            
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos -= pontos_perdidos
+
+            if(rodada == tentativas):
+                print(f"O numero secreto era: {numero_secreto}")
+        
+    print("Fim do jogo")   
+
+if __name__ == '__main__':
+    jogar_adivinacao()
