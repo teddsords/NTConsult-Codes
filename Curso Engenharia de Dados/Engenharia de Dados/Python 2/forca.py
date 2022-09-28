637145f8041e6d9c036182a340421215f98a359e
Teddy Ordoñez <teddyonu@hotmail.com>
2022-09-28 15:59:25 -0300

    Added feedback for wining or loosing the game

--- Curso Engenharia de Dados/Engenharia de Dados/Python 2/forca.py
+++ Curso Engenharia de Dados/Engenharia de Dados/Python 2/forca.py
@@ -4,7 +4,7 @@ def jogar_forca():
     print('   Bem vindo no jogo de Forca')
     print("*******************************")
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'banana'.lower()
     letras_acertadas = ['_', '_', '_', '_', '_', '_']
 
     enforcou = False
@@ -13,19 +13,25 @@ def jogar_forca():
 
     print(letras_acertadas)
     while (not enforcou and not acertou):
-        chute = str.strip(input('Qual letra?\nDigite:'))
+        chute = str.strip(input('Qual letra?\nDigite:')).lower()
         index = 0
 
         if chute in palavra_secreta:
             for letra in palavra_secreta:
-                if chute.lower() == letra.lower():
+                if chute == letra:
                     letras_acertadas[index] = letra.lower()
                 index += 1
         else:
             erros += 1
-            
+        
+        enforcou = erros == 6   # Se erros for igual a 6, ele fara que enforcou seja igual a True
+        acertou = '_' not in letras_acertadas   # Se ha underscores dentro de letras acertadas, sera False
         print(letras_acertadas)    
 
+    if acertou:
+        print('Voce ganhou!')
+    else:
+        print('Voce perdeu!')
     print('Fim do jogo')
 
 if __name__ == '__main__':
