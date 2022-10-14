04423013bbf7234894611370a89dcdd98f1a27eb
Teddy Ordoñez <teddyonu@hotmail.com>
2022-10-14 16:38:21 -0300

    twitter dag is done

--- Curso Engenharia de Dados/Engenharia de Dados/Getting to know Apache Airflow/datapipeline/airflow/dags/twitter_dag.py
+++ Curso Engenharia de Dados/Engenharia de Dados/Getting to know Apache Airflow/datapipeline/airflow/dags/twitter_dag.py
@@ -4,12 +4,19 @@ from datetime import datetime
 from os.path import join
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.utils.dates import days_ago
+from pathlib import Path
 
 ARGS= {
     'owner': 'airflow',
     "depends_on_past": False,
     'start_date': days_ago(6)
 }
+
+BASE_FOLDER= join(
+    str(Path('~\Documents').expanduser()),
+    "/datapipeline/datalake{stage}/twitter_aluraonline/{partition}"
+)
+PARTITION_FOLDER= 'extract_date={{ ds }}'
 TIMESTAMP_FORMAT= '%Y-%m-%dT%H:%M:%S.00Z'
 with DAG(
     dag_id= "twitter_dag",
@@ -22,10 +29,8 @@ with DAG(
         task_id= 'twitter_aluraonline',
         query='aluraOnline',
         file_path= join(
-            "/home/teddy/NTConsult-Codes/Curso Engenharia de Dados/Engenharia de Dados/Getting to know Apache Airflow/datapipeline/datalake", # Criando diretorio
-            "twitter_aluraonline",  # Tabela dentro do nosso "data lake"
-            "extract_date={{ ds }}",    # Partição dentro da tabela com a data de extração
-            "AluraOnline_{{ ds_nodash}}.json"
+            BASE_FOLDER.format(stage= 'bronze', partition= PARTITION_FOLDER),     # Criando diretorio
+            "AluraOnline_{{ ds_nodash }}.json"
         ),
         start_time=(
             '{{'
@@ -41,13 +46,16 @@ with DAG(
 
     twitter_transform = SparkSubmitOperator(
         task_id= 'transform_twitter_aluraonline',
-        application= 'D:\OneDrive - NTCONSULT\NTConsult-Codes\Curso Engenharia de Dados\Engenharia de Dados\Getting to know Apache Airflow\datapipeline\spark\\transformation.py',
+        application= join(
+            str(Path(__file__).parents[2]),
+            'spark/transformation.py'
+        )
         name= 'twitter_transformation',
         application_args= [
             '--src',
-            'D:\OneDrive - NTCONSULT\NTConsult-Codes\Curso Engenharia de Dados\Engenharia de Dados\Getting to know Apache Airflow\datapipeline\datalake\\bronze\\twitter_aluraonline\extrac_date=2022-10-06',
+            BASE_FOLDER.format(stage= 'bronze', partition= PARTITION_FOLDER),
             '--dest',
-            'D:\OneDrive - NTCONSULT\NTConsult-Codes\Curso Engenharia de Dados\Engenharia de Dados\Getting to know Apache Airflow\datapipeline\datalake\silver\\twitter_aluraonline',
+            BASE_FOLDER.format(stage= 'silver', partition= ''),
             '--process-date',
             '{{ ds }}'
         ]
