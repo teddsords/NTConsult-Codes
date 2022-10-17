10fbcf9ea0cd585509e2f2ac9e01b2ef272d6ca8
Teddy Ordoñez <teddyonu@hotmail.com>
2022-10-17 17:27:15 -0300

    Reading dengue dataset

--- Curso Engenharia de Dados/Engenharia de Dados/Apache Beam/main.py
+++ Curso Engenharia de Dados/Engenharia de Dados/Apache Beam/main.py
@@ -1,8 +1,13 @@
-from ast import arg
 import apache_beam as beam
+from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
 pipeline_options = PipelineOptions(argv= None)   # Para receber as opções da Pipeline a ser utilizada
 pipeline= beam.Pipeline(options= pipeline_options)  # Criada a pipeline recebendo as opções anteriormente definidas
 
+dengue = (
+    pipeline
+    | 'Leitura do dataset de dengue' >> 
+        ReadFromText('casos_dengue.txt', skip_header_lines=1)
+)
