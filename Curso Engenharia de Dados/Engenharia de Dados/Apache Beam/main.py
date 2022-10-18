0e6e2c5b412070122b031e8a4b9b6ef6cb246f63
Teddy Ordoñez <teddyonu@hotmail.com>
2022-10-18 16:23:09 -0300

    List to dictionary in pcollection

--- Curso Engenharia de Dados/Engenharia de Dados/Apache Beam/main.py
+++ Curso Engenharia de Dados/Engenharia de Dados/Apache Beam/main.py
@@ -10,9 +10,25 @@ def texto_para_lista(elemento, delimitador="|"):
 
     return elemento.split(delimitador)
 
+def lista_para_dicionario(elemento, colunas):
+    """
+    Recebe duas listas (uma com os valores e outra com as chaves)
+    Retorna um dicionário
+    """
+    return dict(zip(colunas, elemento))
+
 
 pipeline_options = PipelineOptions(argv= None)   # Para receber as opções da Pipeline a ser utilizada
 pipeline= beam.Pipeline(options= pipeline_options)  # Criada a pipeline recebendo as opções anteriormente definidas
+colunas_dengue = [  'id',
+                    'data_iniSE',
+                    'casos',
+                    'ibge_code',
+                    'cidade',
+                    'uf',
+                    'cep',
+                    'latitude',
+                    'longitude']
 
 dengue = (
     pipeline
@@ -20,6 +36,8 @@ dengue = (
         ReadFromText('casos_dengue.txt', skip_header_lines=1)
     | 'De texto para lista' >>
         beam.Map(texto_para_lista)
+    | 'De lista para dicionario' >>
+        beam.Map(lista_para_dicionario, colunas_dengue)
     | 'Mostrar resultados' >> beam.Map(print)
 )
 
