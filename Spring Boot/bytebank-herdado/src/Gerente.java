750acb64bdab8a324e24eb39281c1b35ff7a13d7
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 20:49:18 -0300

    Using interfaces

--- Spring Boot/bytebank-herdado/src/Gerente.java
+++ Spring Boot/bytebank-herdado/src/Gerente.java
@@ -1,4 +1,20 @@
-public class Gerente extends Autenticavel{
+public class Gerente extends Funcionario implements Autenticavel{
+
+    private AutenticacaoUtil autenticador;
+
+    public Gerente(){
+        this.autenticador = new AutenticacaoUtil();
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        return this.autenticador.autentica(senha);
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.autenticador.setSenha(senha);
+    }
 
     public double getBonificacao(){
         System.out.println("Chamando metodo de boni do Gerente");
