428e5b507fbb7d36fa50833ad250d54e452ff50c
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 20:50:27 -0300

    authenticating with system

--- Spring Boot/bytebank-herdado/src/Main.java
+++ Spring Boot/bytebank-herdado/src/Main.java
@@ -8,12 +8,7 @@ public class Main {
         g1.setSalario(5000.0);
         g1.setSenha(2222);
 
-        System.out.println(g1.getNome());
-        System.out.println(g1.getCpf());
-        System.out.println(g1.getSalario());
-
-        System.out.println(g1.autentica(2222));
-
-        System.out.println(g1.getBonificacao());
+        SistemaInterno si = new SistemaInterno();
+        si.autentica(g1);
     }
 }
