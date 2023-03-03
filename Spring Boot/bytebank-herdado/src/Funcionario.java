cd5d9bb305883eea09e3499027b7486b193f756e
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 09:49:24 -0300

    Abstracting class and method

--- Spring Boot/bytebank-herdado/src/Funcionario.java
+++ Spring Boot/bytebank-herdado/src/Funcionario.java
@@ -1,11 +1,9 @@
-public class Funcionario {
+public abstract class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
 
-    public double getBonificacao(){
-        return  this.salario * .05;
-    }
+    public abstract double getBonificacao();
 
     public String getNome() {
         return nome;
