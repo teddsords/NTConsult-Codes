f5277770105ce738b1cb9117809e024dcd9316dd
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 09:50:00 -0300

    simple changes

--- Spring Boot/bytebank-herdado/src/TesteReferencia.java
+++ Spring Boot/bytebank-herdado/src/TesteReferencia.java
@@ -4,12 +4,12 @@ public class TesteReferencia {
         g.setNome("Marcos");
         g.setSalario(5000.0);
 
-        Funcionario f = new Funcionario();
-        f.setSalario(2500.0);
+        EditorVideo ev = new EditorVideo();
+        ev.setSalario(2500.0);
 
         ControleBonificacao controle = new ControleBonificacao();
         controle.registra(g);
-        controle.registra(f);
+        controle.registra(ev);
 
         System.out.println(controle.getSoma());
     }
