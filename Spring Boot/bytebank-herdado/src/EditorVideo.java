7f55f961e27162cf1fac3a26935860a011a804b5
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 09:49:06 -0300

    Video editor class

---
+++ Spring Boot/bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,6 @@
+public class EditorVideo extends Funcionario {
+    public double getBonificacao(){
+        System.out.println("Chamando metodo de boni do Editor de Video");
+        return 100;
+    }
+}
