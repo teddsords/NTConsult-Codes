e38173770054978a7b9ecaef2badc851c415321f
Teddy Ordoñez <teddyonu@hotmail.com>
2023-03-03 21:17:44 -0300

    Added tax calculations for account

--- Spring Boot/bytebank/src/ContaCorrente.java
+++ Spring Boot/bytebank/src/ContaCorrente.java
@@ -1,4 +1,4 @@
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel{
     
     public ContaCorrente(int agencia, int numero){
         super(agencia, numero);
@@ -13,5 +13,10 @@ public class ContaCorrente extends Conta {
     @Override
     public void deposita(double valor) {
         super.saldo += valor;
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.saldo * 0.01;
     }   
 }
